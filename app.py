e9d82d676f4621d2decd99828d37d78a7e980a35
Pranjal-png <pranjaldalal2004@gmail.com>
2025-09-06 19:06:03 +0530

    Fix NLTK LookupError and improve transform_text for Streamlit

--- app.py
+++ app.py
@@ -5,9 +5,16 @@ import nltk
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 
-# Download NLTK data at runtime (required for Streamlit Cloud)
-nltk.download('punkt')
-nltk.download('stopwords')
+# Ensure NLTK data is available
+try:
+    nltk.data.find('tokenizers/punkt')
+except LookupError:
+    nltk.download('punkt')
+
+try:
+    nltk.data.find('corpora/stopwords')
+except LookupError:
+    nltk.download('stopwords')
 
 ps = PorterStemmer()
 
@@ -15,25 +22,11 @@ def transform_text(text):
     text = text.lower()
     text = nltk.word_tokenize(text)
 
-    y = []
-    for i in text:
-        if i.isalnum():
-            y.append(i)
+    y = [i for i in text if i.isalnum()]
+    text = [i for i in y if i not in stopwords.words('english') and i not in string.punctuation]
+    text = [ps.stem(i) for i in text]
 
-    text = y[:]
-    y.clear()
-
-    for i in text:
-        if i not in stopwords.words('english') and i not in string.punctuation:
-            y.append(i)
-
-    text = y[:]
-    y.clear()
-
-    for i in text:
-        y.append(ps.stem(i))
-
-    return " ".join(y)
+    return " ".join(text)
 
 # Load the pre-trained model and vectorizer
 tfidf = pickle.load(open('vectorizer.pkl','rb'))
@@ -44,11 +37,14 @@ st.title("Email/SMS Spam Classifier")
 input_sms = st.text_area("Enter the message")
 
 if st.button('Predict'):
-    transformed_sms = transform_text(input_sms)
-    vector_input = tfidf.transform([transformed_sms])
-    result = model.predict(vector_input)[0]
+    if input_sms.strip() != "":
+        transformed_sms = transform_text(input_sms)
+        vector_input = tfidf.transform([transformed_sms])
+        result = model.predict(vector_input)[0]
 
-    if result == 1:
-        st.header("Spam")
+        if result == 1:
+            st.header("Spam")
+        else:
+            st.header("Not Spam")
     else:
-        st.header("Not Spam")
+        st.warning("Please enter a message to classify.")
