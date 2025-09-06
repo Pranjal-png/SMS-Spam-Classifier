f760ab7d558d25f8e4c4c6129b040643810d9981
Pranjal-png <pranjaldalal2004@gmail.com>
2025-09-06 16:46:25 +0530

    Fix NLTK lookup error by downloading punkt and stopwords

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 import streamlit as st
 import pickle
+import nltk
+
+# Download required NLTK datasets
+nltk.download('punkt')
+nltk.download('stopwords')  # if you are using stopwords anywhere
+
 import string
 from nltk.corpus import stopwords
-import nltk
+
 from nltk.stem.porter import PorterStemmer
 
 ps = PorterStemmer()
